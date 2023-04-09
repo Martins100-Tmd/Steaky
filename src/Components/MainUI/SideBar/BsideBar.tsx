90f6831ccae0d93e2eb2dc61f352ac7f7c6bbebc
Martins100-Tmd <olaitanfabayo@gmail.com>
2023-04-09 18:08:47 +0100

    Done with UI

--- src/Components/MainUI/SideBar/BsideBar.tsx
+++ src/Components/MainUI/SideBar/BsideBar.tsx
@@ -5,33 +5,28 @@ const BSideBarComponent = () => {
   let { Cart } = useContext(CartContext);
   return (
     <div
-      className={`flex flex-row justify-between items-center w-full shadow-xl bg-gray-200 py-5 px-3 duration-500
+      className={`flex flex-row justify-between items-center w-full shadow-xl dark:bg-gray-100 bg-black bg-opacity-90 dark:bg-opacity-100 py-5 px-3 duration-500
        h-full`}
     >
-      <div className="sm:flex flex-col items-start w-10 mt-10 hidden">
-        <div className={`w-full h-[2px] bg-black font-black`}></div>
-        <div className={`w-3/4 h-[2px] bg-black my-2 font-black`}></div>
-        <div className={`w-1/2 h-[2px] bg-black font-black`}></div>
-      </div>
       <div className="relative flex flex-col">
-        <p className="text-lg font-pop absolute bottom-4 left-4">
+        <p className="text-lg font-pop absolute bottom-4 left-4 dark:text-gray-600 text-white">
           {Cart.CartArray.length}
         </p>
         <Link
           to={"/CARTDESKTOP"}
-          className="flex justify-center text-3xl fa relative"
+          className="flex justify-center text-3xl fa relative dark:text-gray-600 text-white"
         >
           &#xf07a;
         </Link>
       </div>
       <i
-        className="flex justify-center my-3 text-3xl fa"
+        className="flex justify-center my-3 text-3xl fa dark:text-gray-600 text-white"
         title={"notifications"}
       >
         &#xf0f3;
       </i>
-      <i className="fa text-3xl">&#xf147;</i>
-      <Link to={"/"} className="fa text-3xl">
+      <i className="fa text-3xl dark:text-gray-600 text-white">&#xf147;</i>
+      <Link to={"/"} className="fa text-3xl dark:text-gray-600 text-white">
         &#xf015;
       </Link>
     </div>
